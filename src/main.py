dd20422df2930555b759f91f5d716bb2d7f25f63
gauranshkumar <gauranshk21@gmail.com>
2021-10-27 09:36:58 +0530

    Working Audio

--- src/main.py
+++ src/main.py
@@ -3,8 +3,10 @@ import magic
 import os
 from typing import MutableSequence
 from fastapi import FastAPI, File, UploadFile
-
+from core import AudioAnalyzer
 app = FastAPI()
+audioAnalyzer = AudioAnalyzer()
+
 
 @app.get("/")
 def read_root():
@@ -25,14 +27,14 @@ async def create_upload_file(file: UploadFile = File(...)):
     filename = file.filename
     validation = mime.from_file(f'./{filename}')
     if validation.find('video') != -1:
+
         return {"filename": filename}
     else:
         os.remove(f'./{filename}')
         return {"Not a valid file format!"}
 
-    
 
 # 1. get and post request
-# 2. post accept data from byte stream now apply validation formats of video + size limit of 25 MBs 
-#     Store data (locally) 
+# 2. post accept data from byte stream now apply validation formats of video + size limit of 25 MBs
+#     Store data (locally)
 # 3. Json (of etc etc)
